d27b1d6c948c2d92ba6aea4c64dd9b7b2073b12c
smcrownhart <103695112+smcrownhart@users.noreply.github.com>
2025-03-04 23:06:36 -0800

    Adding a product now works. Still working on update/delete/search

--- Gourmet Shop/Admin.WebApp/Controllers/Suppliers.cs
+++ Gourmet Shop/Admin.WebApp/Controllers/Suppliers.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GourmetShop.DataAccess.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Admin.WebApp.Controllers
 {
     public class Suppliers : Controller
     {
-        public IActionResult Index()
+        private readonly ISupplierRepository _supplierRepository;
+
+        public Suppliers(ISupplierRepository supplierRepository)
         {
-            return View();
+            _supplierRepository = supplierRepository;
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public async Task<IActionResult> Index()
+        {
+            var suppliers = await _supplierRepository.GetAllAsync();
+            return View(suppliers);
         }
     }
 }
