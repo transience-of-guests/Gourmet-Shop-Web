79275ccd706cefd0dbe31958dd1da15ad426bae2
jmai136 <jude.mai@student.centralia.edu>
2025-03-06 09:27:06 -0800

    fix: delete unused GourmetShop.WebApp.Admin project as well as GourmetShop.sql script

--- Gourmet Shop/GourmetShop.WebApp.Admin/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GourmetShop.WebApp.Admin.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
